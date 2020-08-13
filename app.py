638003ef376af3ac885a0c89bb7897851c27892a
Thiago Casa Nova <tcnl@cin.ufpe.br>
2020-08-12 21:22:20 -0300

    updating first map view

--- app.py
+++ app.py
@@ -4,13 +4,10 @@ import numpy as np
 import pydeck as pdk
 import plotly.express as px
 
-DATA_URL = (
-"/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
-)
+DATA_URL = "./Motor_Vehicle_Collisions_-_Crashes.csv"
 
 st.title("Motor Vehicle Collision in New York City")
-st.markdown("This application is a Streamlit dashboard that can be used "
-"to analyze motor vehicle collision in NYC 🗽💥🚗")
+st.markdown("This application is a Streamlit dashboard that can be used to analyze motor vehicle collision in NYC 🗽💥🚗")
 
 @st.cache(persist=True)
 def load_data(nrows):
@@ -26,8 +23,28 @@ original_data = data
 
 st.header("Where are the most people injured in NYC?")
 injured_people = st.slider("Number of persons injured in vehicle collisions", 0, 19)
-st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
-
+midpoint = (np.average(data['latitude']), np.average(data['longitude']))
+st.write(pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state={
+        "latitude": midpoint[0],
+        "longitude": midpoint[1],
+        "zoom": 11,
+        "pitch": 50,
+    },
+    layers=[
+        pdk.Layer(
+        "HexagonLayer",
+        data=data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"),
+        get_position=['longitude', 'latitude'],
+        radius=300,
+        extruded=True,
+        pickable=True,
+        elevation_scale=4,
+        elevation_range=[0, 1000],
+        ),
+    ]
+))
 
 st.header("How many collision occur during a given time of day?")
 hour = st.slider("Hour to look at", 0, 23)
@@ -72,23 +89,10 @@ select = st.selectbox('Affected type of people', ['Pedestrian', 'Cyclists', 'Mot
 
 if select == 'Pedestrians':
     st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending=False).dropna(how='any')[:5])
-
 elif select == 'Cyclists':
     st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending=False).dropna(how='any')[:5])
-
 else:
     st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=['injured_motorists'], ascending=False).dropna(how='any')[:5])
-
-
-
-
-
-
-
-
-
-
-
 
 if st.checkbox("Show Raw Data", False):
     st.subheader('Raw Data')
